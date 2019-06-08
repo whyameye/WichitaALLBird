be297b1efab8ed336dc004541b4cb87755e0ee27
John Harrison <whyameye@yahoo.com>
2019-06-07 23:46:50 -0500

    enhanced serial test to brighten and dim (untested)

--- serialTest.py
+++ serialTest.py
@@ -1,9 +1,10 @@
 import serial, time
+from itertools import chain
+
 # ser = serial.Serial('/dev/cu.usbserial-14120', 115200)
 ser = serial.Serial('/dev/cu.wchusbserial14120', 115200)
 
 def setLED(strand, numOnStrand, color, amplitude):
-    # ser.write(serial.to_bytes([0x01,150,0x7f,0xff]))                       
     bytes = []
     bytes.append((strand << 3) + numOnStrand)
     bytes.append(color)
@@ -19,7 +20,7 @@ def test1(amp):
                 time.sleep(.001)
 
 def test2(color):
-    for amp in range(128):
+    for amp in chain(range(128),range(128,0,-1)):
         for strand in range(18):
             for num in range(5):
                 setLED(strand, num, color, amp)

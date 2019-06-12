3524f945ed9cda9b449c2b6dc53ac86addf5be94
john <john@filimin.com>
2019-06-11 22:45:29 -0500

    add strand test code

--- serialTest.py
+++ serialTest.py
@@ -1,9 +1,23 @@
-import serial, time
+import serial, time, sys, select, serial.tools.list_ports
 from itertools import chain
 
-# ser = serial.Serial('/dev/cu.usbserial-14120', 115200)
-ser = serial.Serial('/dev/cu.wchusbserial14120', 115200)
 
+# ser = serial.Serial('/dev/cu.wchusbserial14120', 115200)
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+def printPorts():
+    ports = serial.tools.list_ports.comports()
+    for port, desc, hwid in sorted(ports):
+        print("{}".format(port))
+
+
+def getInput(timeToWait):
+    i, o, e = select.select( [sys.stdin], [], [], timeToWait )
+    if (i):
+        print(sys.stdin.readline().strip())
+        return True
+    return False
+    
 def setLED(strand, numOnStrand, color, amplitude):
     bytes = []
     bytes.append((strand << 3) + numOnStrand)
@@ -25,3 +39,34 @@ def test2(color):
             for num in range(5):
                 setLED(strand, num, color, amp)
                 time.sleep(.001)
+
+
+def testStrand():
+    while True:
+        print()
+        print("Ports available: ")
+        printPorts()
+        port = input("Port #: ")
+        ser = serial.Serial('/dev/ttyUSB'+port, 115200)
+        arduinoID = 1 # TODO
+        while True:
+            print("Port "+port+" corresponds to Arduino ID "+str(arduinoID))
+            strand = input("Strand ('<ENTER>' to reenter port): ")
+            if strand == "":
+                ser.close()
+                break
+            print("Flashing Arduino ID "+str(arduinoID)+" strand "+strand+". <ENTER> to stop.")
+            strand = int(strand)
+            while True:
+                for i in range(5):
+                    setLED(strand, i, 150, 254)
+                    time.sleep(.001)
+                time.sleep(1)
+                for i in range(5):
+                    setLED(strand,i, 150, 0)
+                    time.sleep(.001)
+                if getInput(1):
+                    break
+
+
+

7213b35a90a1e741fc120b357b09c2e331f98112
john <john@filimin.com>
2019-11-02 20:27:01 -0500

    first LED in mode seems to be working

--- main.py
+++ main.py
@@ -2,24 +2,7 @@ import json, time, random
 import serialTest
 
 DEBUG = True
-
-def saveBirds(birds):
-    f = open("birds.json","w")
-    f.write(json.dumps(birds, indent=4))
-    f.close()
-    
-def loadJSON(filename):
-    f = open(filename,"r")
-    jsonResult = json.loads(f.read())
-    f.close()
-    return jsonResult
-
-def debug(str):
-    if DEBUG:
-        print("DEBUG: "+str)
-
-def millis():
-    return int(round(time.time() * 1000))
+CHANCE_OF_PICKING_CLOSEST_Y = 0.8
 
 class Seq(object):
     activeLeds = []
@@ -27,50 +10,123 @@ class Seq(object):
     def __init__(self, mode):
         self.mode = mode
         self.startTime = millis()
-        self.leds = []
+        self.leds = [] # list of LEDs we're currently controlling
         self.totalLeds = random.randrange(self.mode['minLength'],
                                           self.mode['maxLength']+1)
         debug("total LEDs for new sequence: "+str(self.totalLeds))
-        self.currentLed = 0
+        self.currentLedIndex = 0 # number of active LEDs in mode
+        self.direction = "" #
         
     def remove(self):
         pass
 
-    def addLed(self,leds):
-        deltaTime = millis() - self.startTime
-        if ((deltaTime >= (self.currentLed * self.mode['timeToNextLed'])) and
-            (self.currentLed < self.totalLeds)):
-            debug("adding LED:")
-            if self.currentLed == 0:
-                ledIndex = self.getStartLed()
-                debug("adding first led: "+str(ledIndex))
-            else:
-                # FIXME: how to choose ledIndex
-                ledIndex = (self.leds[self.currentLed-1][0]+1) % len(ledSequence)
-                debug("adding additional led: "+str(ledIndex))
-            if ledIndex in self.activeLeds:
-                if self.currentLed == 0:
-                    debug("1st LED already in list. Kill the sequence")
-                    return False
-                else:
-                    debug("found LED already in another active sequence. Stunt")
-                    self.totalLeds = self.currentLed
-                    return True
-            self.activeLeds.append(ledIndex)
-            debug("activeLEDs: "+str(self.activeLeds));
-            self.leds.append([ledIndex, millis()])
-            self.currentLed += 1
+    def getStartLed(self):
+        """get first LED in sequence
+        returns LED to light described as list: [strand, index of y element in strand]
+        """
+
+        possibleStartRings = self.mode["moves"]["startRings"]
+        possibleStartGroups = self.mode["moves"]["startGroups"]
+        possibleStrandsInRings = []
+        for i in possibleStartRings:
+            possibleStrandsInRings += strandsInRing[i]
+        possibleStrandsInGroups = []
+        for i in possibleStartGroups:
+            possibleStrandsInGroups += strandsInGroup[i]
+        possibleStrands = list(set(possibleStartRings) & set(possibleStartGroups))
+        theChosenStrandIndex = random.randrange(0,len(possibleStrands))
+        theChosenStrand = possibleStrands[theChosenStrandIndex]
+        lenStartMoves = len(self.mode["moves"]["startDirection"])
+        self.direction = self.mode["moves"]["startDirection"][random.randrange(0,lenStartMoves)]
+        LedIndexOnStrand = random.randrange(0,len(strands[theChosenStrand]["y"])+1)
+        return [theChosenStrand, LedIndexOnStrand]
+
+    def getViableMove(self):
+        currentLED = self.activeLEDs[-1] # get last LED list in list of LED lists 
+        currentStrand = currentLED[0]
+        currentLEDIndexOnStrand = currentLED[1]
+        if self.direction == "left" or self.direction == "right":
+            try:
+                possibleStrands = strands[currentStrand][self.direction].copy()
+            except KeyError:
+                return False
+            while len(possibleStrands) > 0:
+                i = random.randrange(0,len(possibleStrands))
+                strandToTry = possibleStrands.pop(i)
+                if testViableMove(currentStrand, strandToTry):
+                    return chooseLEDOnStrand(currentStrand, strandToTry)
+            return False
+        if self.direction == "up" and currentLEDOnStrand > 0:
+            return [currentStrand, currentLEDOnStrand - 1]
+        if self.direction == "down" and currentLEDOnStrand < len(strands[currentStrand]["y"]):
+            return [currentStrand, currentLEDOnStrand + 1]
         return False
 
-    def getStartLed():
-        startRings = self.modemode["moves"]["startRings"]
-        startGroups = self.modemode["moves"]["startGroups"]
-        while True:
-            testStrand = random.randrange(0,len(strands)+1)
-            if (set(startRings) & set(strands[str(testStrand)]["ring"]) &&
-                set(startGroups) & set(strands[str(testStrand)]["group"])):
-                break;
-        return [testStrand, random.randrange(0,len(strands[str(testStrand)]["y"])+1)]
+    def testViableMove(self,currentStrand, strandToTry):
+        if (modes[mode]["moveOutsideGroup"] == "N" and
+            strands[currentStrand]["group"] != strands[strandToTry]["group"]):
+            return False
+        if (modes[mode]["moveOutsideRing"] == "N" and
+            strands[currentStrand]["ring"] != strands[strandToTry]["ring"]):
+            return False
+        
+
+    def getAdditionalLed(self):
+        """find an Led that meets the criteria
+        randomly trying different directions if needed
+        """
+        viableMove = getViableMove() if random.random > modes[self.mode]["changeMove"] else False
+        if viableMove == False:
+            movesAllowed = modes[self.mode]["movesAllowed"].copy()
+            numMovesAllowed = len(movesAllowed)
+
+        while viableMove == False:
+            if numMovesAllowed == 0:
+                return False
+            self.direction = movesAllowed.pop(random.randrange(0,numMovesAllowed))
+            viableMove = getViableMove()
+            numMovesAllowed -= 1
+        return viableMove
+
+    def addLed(self,leds):
+        """add first or next LED in sequence
+        returns True if LED was successfully added, otherwise False
+        """
+        deltaTime = millis() - self.startTime
+        if ((deltaTime < (self.currentLedIndex * self.mode['timeToNextLed'])) or
+            (self.currentLedIndex >= self.totalLeds)):
+            return False
+        
+        if self.currentLedIndex == 0:
+            currentLed = self.getStartLed() # list of 2 elements
+            debug("added first led: "+str(currentLed))
+
+        else:
+            currentLed = self.getAdditionalLed()
+            debug("added additional led: "+str(currentLed))
+        if currentLed in self.activeLeds:
+            if self.currentLedIndex == 0:
+                debug("1st LED already in list. Kill the sequence")
+                return False
+            else:
+                debug("found LED already in another active sequence. Stunt")
+                self.totalLeds = self.currentLedIndex
+                return False
+        self.activeLeds.append(ledIndex)
+        debug("activeLEDs: "+str(self.activeLeds));
+        self.leds.append([ledIndex, millis()])
+        self.currentLedIndex += 1
+
+
+    def chooseLEDOnStrand(currentStrand, strandToTry):
+        if (random.random > CHANCE_OF_PICKING_CLOSEST_Y):
+            return [strandToTry, random.randrange(0,len(strands[testStrand]["y"])+1)]
+        CurrentLEDPos = strands[currentLED[0]]["y"][currentLED[1]]
+        PossibleLEDPoses - strands[strandToTry]["y"]
+        distances = [abs(i - CurrentLEDPos) for i in PossibleLEDPoses]
+        return distances.index(min(distances))
+                         
+        
 
     def updateLed(self,led):
         deltaTime = millis() - led[1]
@@ -92,11 +148,11 @@ class Seq(object):
         # strip.setPixelColor(ledSequence[led[0]],Color(theColors[0],theColors[1],theColors[2]))
         return inPattern
 
-    # continually call this high-level update method
-    # returns False if all Leds have gone through all states
     def update(self):
-        if not self.addLed(self.leds):
-            return False # FIXME: false returned only when stunted, I think? Why kill everything in that case?
+        """continually call this high-level update method
+        returns False if all Leds have gone through all states
+        """
+        self.addLed(self.leds)
         for i in range(len(self.leds)):
             if (not self.updateLed(self.leds[i])):
                 # debug("updateLed: "+str(self.leds[i]))
@@ -110,6 +166,49 @@ class Seq(object):
                         return False
         return True
 
+
+def saveBirds(birds):
+    f = open("birds.json","w")
+    f.write(json.dumps(birds, indent=4))
+    f.close()
+    
+def loadJSON(filename):
+    f = open(filename,"r")
+    jsonResult = json.loads(f.read())
+    f.close()
+    return jsonResult
+
+def debug(str):
+    if DEBUG:
+        print("DEBUG: "+str)
+
+def millis():
+    return int(round(time.time() * 1000))
+
+def loadGlobalVariables():
+    """create list of integers representing strand #s depending on what ring and group they are in
+    """
+    global strandsInRing, strandsInGroup, modes, strands
+
+    jsonModes = loadJSON("WichitaALLModes.json")
+    jsonStrands = loadJSON("WichitaALLStrands.json")
+
+    # indexes that are numeric strings are annoying, so we convert:
+    for i in jsonModes:
+        modes[int(i)] = jsonModes[i]
+    for i in jsonStrands:
+        strands[int(i)] = jsonStrands[i]
+
+    # make a list of which strands are in which rings and groups:
+    for i in range(4):
+        strandsInRing.append([])
+        strandsInGroup.append([])
+    for i in strands:
+        for j in range(1,4):
+            if j in strands[i]["ring"]:
+                strandsInRing[j].append(i)
+            if j in strands[i]["group"]:
+                strandsInGroup[j].append(i)
 
 def generateSequences(mode, newTrig):
     numOfSeqs = random.randrange(mode['minInstances'],mode['maxInstances']+1)
@@ -125,13 +224,16 @@ def updateMode():
 if __name__ == '__main__':
 
     # setup
-    mode = 0
     allTheSeqs = []
     newTrig = False
-    modes = loadJSON("WichitaALLModes.json")
-    strands = loadJSON("WichitaALLStrands.json")
+    modes = {}
+    strands = {}
+    strandsInRing = []
+    strandsInGroup = []
+    loadGlobalVariables()
 
-    generateSequences(modes[str(mode)], newTrig)
+    # start in Mode 0
+    generateSequences(modes[0], newTrig)
     trigTime = 0
     
     # loop
@@ -153,6 +255,3 @@ if __name__ == '__main__':
                 # mode = ((millis() - trigTime) < TRIGGER_LENGTH) TODO
                 generateSequences(modes[mode], newTrig)
         # strip.show()
-
-            
-            
